08c3d9663564df942f9fe133502cda4868dc5c40
Regina8023 <meow@umich.edu>
2019-03-23 02:15:20 -0400

    test

--- train.py
+++ train.py
@@ -75,3 +75,4 @@ if __name__ == '__main__':
 
         print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
         model.update_learning_rate()                     # update learning rates at the end of every epoch.
+
